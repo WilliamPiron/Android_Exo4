10b8571f07dc198b822493fa83f7183b074eecca
wilpiron <william.piron@laposte.net>
2016-12-08 16:38:11 +0100

    Ajout de l'activité de l'alarme

---
+++ app/src/main/java/william/piron/fr/exo4/AlarmActivity.java
@@ -0,0 +1,13 @@
+package william.piron.fr.exo4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
