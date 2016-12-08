8cd27e63920107aa954f8f65eeeba610cbd2b7e5
wilpiron <william.piron@laposte.net>
2016-12-08 17:41:39 +0100

    AJout de bouts de code (mise à jour du code final à venir)

--- app/src/main/java/william/piron/fr/exo4/AlarmActivity.java
+++ app/src/main/java/william/piron/fr/exo4/AlarmActivity.java
@@ -1,13 +1,21 @@
 package william.piron.fr.exo4;
 
+import android.content.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AlarmActivity extends AppCompatActivity {
+    public static Intent intent;
+    public static TextView tempsText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        intent = getIntent();
+
     }
 }
--- app/src/main/java/william/piron/fr/exo4/MainActivity.java
+++ app/src/main/java/william/piron/fr/exo4/MainActivity.java
@@ -2,13 +2,16 @@ package william.piron.fr.exo4;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-    private AlarmManager alarmManager;
-    private PendingIntent pendingIntent;
+    private static AlarmManager alarmManager;
+    private static PendingIntent pendingIntent;
+    private static Intent intent;
     private static final String TRENTESECONDE_MESSAGE = "30 secondes";
     private static final String UNEMINUTE_MESSAGE = "1 minute";
 
@@ -21,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
         Button button30s = (Button) findViewById(R.id.layoutLinear_button30s);
         Button button60s = (Button) findViewById(R.id.layoutLinear_button60s);
 
+        alarmManager = (AlarmManager) MainActivity.this.getSystemService(MainActivity.this.ALARM_SERVICE);
+        //intent = new Intent(MainActivity.this, AlarmActivity.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
 
+        button30s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button60s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
